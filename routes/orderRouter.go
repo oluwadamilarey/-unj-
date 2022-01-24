158f3fad683b4a5ae94d94e8efd65b8c50d41797
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2022-01-24 07:15:06 +0100

    added BSON primitives

---
+++ routes/orderRouter.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	controller "restaurant-management/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func OrderRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/orders", controller.GetOrder())
+	incomingRoutes.GET("/orders/:order_id", controller.GetOrder())
+	incomingRoutes.POST("/orders", controller.CreateOrder())
+	incomingRoutes.PATCH("/orders/:order_id", controller.UpdateOrder())
+}
